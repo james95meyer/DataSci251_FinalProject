5b18526815f036073162b4fee494ccf234e578bd
james95meyer <james95meyer@gmail.com>
2023-04-01 15:27:56 -0700

    Added Audio_Files folder and updated recorder file

--- Py_Files/Continual_Recorder.py
+++ Py_Files/Continual_Recorder.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pyaudio
 import wave
 import datetime
 import random
-
-
-# In[2]:
 
 
 chunk = 1024  # Record in chunks of 1024 samples
@@ -18,9 +9,6 @@ sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
 fs = 44100  # Record at 44100 samples per second
 seconds = 5
-
-
-# In[4]:
 
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
@@ -40,9 +28,11 @@ while True:
         data = stream.read(chunk)
         frames.append(data)
     print(len(frames))
+    
+    # Save the recorded data as a WAV file
     timestamp = datetime.datetime.now().strftime("%H%M%S")
     filename = "audio_" + timestamp + ".wav"
-    wf = wave.open(filename, 'wb')
+    wf = wave.open("Audio_Files/" + filename, 'wb')
     wf.setnchannels(channels)
     wf.setsampwidth(2)
     wf.setframerate(fs)
@@ -50,13 +40,4 @@ while True:
     wf.close()
     print("Recording saved")
 
-# Stop and close the stream 
-#stream.stop_stream()
-#stream.close()
-# Terminate the PortAudio interface
-#p.terminate()
-
-#print('Finished recording')
-
-# Save the recorded data as a WAV file
 
